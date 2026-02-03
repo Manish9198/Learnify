a5725f969fcd495c14af54dcbe7584875c9a6388
Manish9198 <sutarmanish9198@gmail.com>
2026-02-03 18:35:50 +0530

    Add comments to clarify purpose of StrictMode and server configuration in Vite

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,7 @@ import { CourseContextProvider } from "./context/CourseContext.jsx";
 export const server = "http://localhost:5000";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <React.StrictMode>    //for highlighting potential problems in the application ,for devlopment only
     <UserContextProvider>
       <CourseContextProvider>
         <App />
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // server:{
+  // server:{   // y4e pdte time add kra tha ise uncomment na kre otherwise or scripts me bhi thoda change krna hoga
   //   proxy:{
   //     '/api':'http://localhost:3000',  3000 = should be same as backend port, production ready to make the request from any port on frontend
   //   }
