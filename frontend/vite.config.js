2d8451096ca4f0655bfa204641a0631f8eb634ba
Manish9198 <sutarmanish9198@gmail.com>
2026-02-03 13:42:16 +0530

    Remove commented-out server proxy configuration from vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // server:{
+  //   proxy:{
+  //     '/api':'http://localhost:3000',  3000 = should be same as backend port, production ready to make the request from any port on frontend
+  //   }
+
+  // },
   plugins: [react()],
 })
